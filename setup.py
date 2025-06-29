729bb97d2d2b54efbdd87c3ff5aaea9002e6acb6
davide.cavicchini <davidecavicchini@hotmail.com>
2025-06-28 20:16:05 -0600

    update README

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 # Read version from version.py
 version = {}
-with open(os.path.join("retico_screen", "version.py")) as fp:
+with open(os.path.join("retico_videoPlayback", "version.py")) as fp:
     exec(fp.read(), version)
 
 # Read README for long description
@@ -17,11 +17,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="retico-screen",
+    name="retico-videoPlayback",
     version=version["__version__"],
     author="DavidC001",
     author_email="",
-    description="A ReTiCo module for showing video streams and detected objects on a screen.",
+    description="A ReTiCo module for playing back video files as a video stream source.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
@@ -30,7 +30,7 @@ setup(
         "retico-core",
         # "retico-vision",
     ],
-    keywords="retico, real-time, dialogue, object detection, computer vision, CLIP, feature extraction",
+    keywords="retico, real-time, dialogue, video playback, video stream",
     project_urls={
         
     },
